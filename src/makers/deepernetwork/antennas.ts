73a3ebb4fc7d1d5e60e52f7eda1f974e22721cfe
EtaJx <medianeras57@gmail.com>
2021-10-19 11:02:38 +0800

    add deepernetwork hotspot

---
+++ src/makers/deepernetwork/antennas.ts
@@ -0,0 +1,46 @@
+import { MakerAntenna } from '../antennaMakerTypes'
+
+const DEEPER_NETWORK_US = {
+  name: 'DeeperNetwork Hotspot (US915)',
+  gain: 3,
+} as MakerAntenna
+
+const DEEPER_NETWORK_AS = {
+  name: 'DeeperNetwork Hotspot (AS923)',
+  gain: 3,
+} as MakerAntenna
+
+const DEEPER_NETWORK_AU = {
+  name: 'DeeperNetwork Hotspot (AU915)',
+  gain: 3,
+} as MakerAntenna
+
+const DEEPER_NETWORK_RU = {
+  name: 'DeeperNetwork Hotspot (RU864)',
+  gain: 3,
+} as MakerAntenna
+
+const DEEPER_NETWORK_KR = {
+  name: 'DeeperNetwork Hotspot (KR920)',
+  gain: 3,
+} as MakerAntenna
+
+const DEEPER_NETWORK_CN = {
+  name: 'DeeperNetwork Hotspot (CN470)',
+  gain: 3,
+} as MakerAntenna
+
+const DEEPER_NETWORK_EU = {
+  name: 'DeeperNetwork Hotspot (EU868)',
+  gain: 3,
+} as MakerAntenna
+
+export default {
+  DEEPER_NETWORK_US,
+  DEEPER_NETWORK_AS,
+  DEEPER_NETWORK_KR,
+  DEEPER_NETWORK_RU,
+  DEEPER_NETWORK_AU,
+  DEEPER_NETWORK_CN,
+  DEEPER_NETWORK_EU,
+}
